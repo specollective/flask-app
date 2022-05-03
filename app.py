aea765a3b5ce75d90e450ee1d51d5ddd18ff4b75
Joe Torreggiani <joetorreggiani@gmail.com>
2022-05-02 19:07:08 -0700

    Test cors settings.

--- app.py
+++ app.py
@@ -3,14 +3,23 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, resources={
+CORS(
+  app,
+  resources={
     r"/api/*": {
         "origins": ['http://localhost:3000'],
     }
-})
+  },
+  supports_credentials=True,
+)
 
 @app.route('/api/ping')
 def ping():
     response = jsonify({'ping': 'pong'})
-    response.set_cookie('token', 'example-token')
+    response.set_cookie(
+      key='token',
+      value='example-token',
+      domain='127.0.0.1',
+      path='/'
+    )
     return response
