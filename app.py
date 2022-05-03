275392c90e2436ee1cd2a7389999dda28da7cb3d
Joe Torreggiani <joetorreggiani@gmail.com>
2022-05-02 18:22:31 -0700

    Test basic cors configs.

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
 
-@app.route('/ping')
+CORS(app, resources={
+    r"/api/*": {
+        "origins": ['http://localhost:3000'],
+    }
+})
+
+@app.route('/api/ping')
 def ping():
     response = jsonify({'ping': 'pong'})
+    response.set_cookie('token', 'example-token')
     return response
