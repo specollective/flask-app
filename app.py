85566f3d8dd0593ab540c7181ea6825c072897a1
Joe Torreggiani <joetorreggiani@gmail.com>
2022-05-02 17:55:00 -0700

    Add ping endpoint.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/ping')
+def ping():
+    response = jsonify({'ping': 'pong'})
+    return response
